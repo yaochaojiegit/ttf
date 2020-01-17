ed9407a11f9b9f5daec8537e765720b7bd271996
yaochaojiegit <yaochaojiewy@163.com>
2020-01-17 20:13:07 +0800

    ttf项目：1尝试重构项目，第一次尝试修改

--- src/main/java/com/qf/pojo/CartKey.java
+++ src/main/java/com/qf/pojo/CartKey.java
@@ -1,32 +1,19 @@
 package com.qf.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author 老姚
+ */
+@Setter
+@Getter
+@ToString
 public class CartKey {
+    
     private Integer id;
 
     private Integer pid;
 
-    public CartKey() {
-		super();
-	}
-
-	public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPid() {
-        return pid;
-    }
-
-    public void setPid(Integer pid) {
-        this.pid = pid;
-    }
-
-	@Override
-	public String toString() {
-		return "CartKey [id=" + id + ", pid=" + pid + "]";
-	}
 }
